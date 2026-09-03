d8675765abb0b88de5692918539ec736e3620a40
BenOConnell-Hub <bennjaoconnell@gmail.com>
2026-09-03 21:47:26 +0200

    Fixed issue where Specified_Filter was not assigned

--- Main_Astro_Trip.py
+++ Main_Astro_Trip.py
@@ -14,12 +14,28 @@ import numpy as np
 from astropy.io import fits
 import astropy
 
-PP = Photometry_Pipeline.Photometry_Pipeline(Exposure_Time=5.0, Filter_Type= 'SDSSgp+')
+
+#Dark_Directory = 'Data/2026_09_01/2026-09-01_OMICRON_F3p17_OPF_QHY600Ma_DARK/' #2026-09-01
+Dark_Directory = 'Data/2026_09_03/2026-09-03_OMICRON_F3p17_OPF_QHY600Ma_DARK/'
+#Bias_Directory = 'Data/2026_09_01/2026-09-01_OMICRON_F3p17_OPF_QHY600Ma_BIAS/' #2026-09-01
+Bias_Directory = 'Data/2026_09_03/2026-09-03_OMICRON_F3p17_OPF_QHY600Ma_BIAS/'
+Flat_Directory = 'Data/2026_09_01/2026-09-01_OMICRON_F3p17_OPF_QHY600Ma_FLAT/'
+#Raw_Directory = 'Data/2026_08_31/2026-08-31_OMICRON_F3p17_OPF_QHY600Ma_M31/' #2026-08-31
+Raw_Directory = 'Data/2026_09_02/2026-09-02_OMICRON_F3p17_OPF_QHY600Ma_M-16/'
+
+PP = Photometry_Pipeline.Photometry_Pipeline(Exposure_Time=60, 
+                                             Filter_Type=None,
+                                             Raw_Directory = Raw_Directory,
+                                             Flat_Directory = Flat_Directory,
+                                             Bias_Directory = Bias_Directory,
+                                             Dark_Directory = Dark_Directory)
+'''
 Dark_Directory = 'Data/2026_09_01/2026-09-01_OMICRON_F3p17_OPF_QHY600Ma_DARK/'
 Bias_Directory = 'Data/2026_09_01/2026-09-01_OMICRON_F3p17_OPF_QHY600Ma_BIAS/'
 Flat_Directory = 'Data/2026_09_01/2026-09-01_OMICRON_F3p17_OPF_QHY600Ma_FLAT/'
 Raw_File_Path = "Data/2026_09_01/2026-09-01_OMICRON_F3p17_OPF_QHY600Ma_tres-2/tres-2_20260901T202356977_SC_SDSSgp+_0005s000_000000.fits"
-data = PP.Science_Array(Raw_File_Path, Dark_Directory, Bias_Directory, Flat_Directory)
+'''
+data = PP.Reduce_Images('M31')
 #PP.Calculate_Flat_Master_Array(Flat_Directory, Bias_Directory)
 
 '''
